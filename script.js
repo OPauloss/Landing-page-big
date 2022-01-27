534881b754db9273c5f14f3c2cb06ed173ef3863
Paulo H Santos <paulohenrique.uerj@gmail.com>
2022-01-27 17:24:43 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function contratar(){
+    let contrato = document.querySelector('button')
+    contrato.textContent = `Me contratou!`
+    alert(`Essa foi a melhor decisão da sua vida!`)
+    alert('Uma conta no valor de R$ 500.000,00 chegará em breve no endereço do usuário.')
+}
+function conheça(){
+    alert('Tem que pagar')
+}
+function dinheiro(){
+    alert('Desculpe, você não tem dinheiro suficiente para visualizar o conteúdo dessa página. Volte quando for rico.')
+}
+function pechincha(){
+    alert('Pague o valor integral sempre. Doguezinglsh merece.')
+}
+function lambidinha(){
+    alert('Grátis na primeira lambidinha. R$ 900.000,00 para cada lambidinha adicional.')
+}
